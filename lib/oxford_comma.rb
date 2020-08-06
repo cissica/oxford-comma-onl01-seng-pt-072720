aa899e9e1349df9e9e5c2066e945186c25502653
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-06 09:00:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,9 @@ elsif array.size == 2
   string2 << array.join(" and ")
   return string2
 elsif array.size == 3
-  string3 = ""
-  string3 << array.join(", ") 
-  return string3
+ string3 = ""
+ string3 << array.join(", ")
+ string3 << array.insert( -1, "and")
+ return string3
   end
 end
